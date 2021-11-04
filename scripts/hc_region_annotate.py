90a485e0ba42a05ab78c844e477530919b589ce4
Adam English <ACEnglish@gmail.com>
2021-11-03 20:15:54 -0500

    Confidence Region scripts

---
+++ scripts/hc_region_annotate.py
@@ -0,0 +1,35 @@
+import sys
+import pysam
+import truvari
+from intervaltree import IntervalTree
+from collections import defaultdict
+
+vcf_fn = sys.argv[1]
+bed_fn = sys.argv[2]
+
+lookup = defaultdict(IntervalTree)
+with open(bed_fn, 'r') as fh:
+    for line in fh:
+        chrom, start, end, cov = line.strip().split('\t')
+        start = int(start)
+        end = int(end)
+        cov = int(cov)
+        lookup[chrom].addi(start, end, data=cov)
+
+vcf = pysam.VariantFile(vcf_fn)
+n_header = vcf.header.copy()
+n_header.add_line(('##INFO=<ID=ASMCOV,Number=.,Type=Integer,'
+                   'Description="Coverage of Assemblies of start/end breakpoints">'))
+out = pysam.VariantFile("/dev/stdout", 'w', header=n_header)
+
+for entry in vcf:
+    sz = truvari.entry_size(entry)
+    if sz < 50:
+        continue
+    entry = truvari.copy_entry(entry, n_header)
+    up_pos = list(lookup[entry.chrom].at(entry.start))[0].data
+    dn_pos = list(lookup[entry.chrom].at(entry.stop))[0].data
+    entry.info["ASMCOV"] = [up_pos, dn_pos]
+    out.write(entry)
+        
+
